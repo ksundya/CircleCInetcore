8141d5bfa4550e0b4cf65deb9af794106de756e0
Kseniya Dunchyk <Kseniya_Dunchyk@epam.com>
2019-10-09 14:38:49 +0300

    adding .Net Core Project

---
+++ circleCI/Program.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace circleCI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+	        IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://cloud.google.com");
+            Thread.Sleep(3000);
+            Console.WriteLine("the page is opened");
+            driver.Quit();
+            Console.WriteLine("the page is closed");
+        }
+    }
+}
