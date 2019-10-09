8cdf578e72b709db192c05e9aefed6de7a166610
Kseniya <45160353+ksundya@users.noreply.github.com>
2019-10-09 22:15:53 +0300

    Update Program.cs

--- circleCI/Program.cs
+++ circleCI/Program.cs
@@ -10,7 +10,8 @@ namespace circleCI
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-	        IWebDriver driver = new ChromeDriver();
+	       // IWebDriver driver = new ChromeDriver();
+		 IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
             driver.Navigate().GoToUrl("https://cloud.google.com");
             Thread.Sleep(3000);
             Console.WriteLine("the page is opened");
